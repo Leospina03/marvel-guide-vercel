28ae5bbc47eda92db0cd2415790353a249592b88
Leospina03 <897664@stud.unive.it>
2026-03-20 11:47:49 +0100

    fix: remove cached query from local mcu data hook

--- src/hooks/use-mcu.ts
+++ src/hooks/use-mcu.ts
@@ -1,21 +1,19 @@
-import { useQuery } from "@tanstack/react-query";
 import { mcuData, MCUItem } from "../data/mcu";
 
 export function useMCUList() {
-  return useQuery({
-    queryKey: ["mcu-list"],
-    queryFn: async () => mcuData,
-  });
+  return {
+    data: mcuData,
+    isLoading: false,
+    error: null,
+  };
 }
 
 export function useMCUItem(id: string) {
-  return useQuery({
-    queryKey: ["mcu-item", id],
-    queryFn: async () => {
-      const item = mcuData.find(i => i.id === id);
-      if (!item) throw new Error("Titolo non trovato");
-      return item as MCUItem;
-    },
-    enabled: !!id,
-  });
+  const item = mcuData.find((i) => i.id === id);
+
+  return {
+    data: item as MCUItem | undefined,
+    isLoading: false,
+    error: item ? null : new Error("Titolo non trovato"),
+  };
 }
